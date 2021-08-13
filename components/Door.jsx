054490f9fbee6094aa58b1ab0aebf01e1af577a2
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-13 16:22:15 -0300

    Finalizando estilos do componente porta

--- components/Door.jsx
+++ components/Door.jsx
@@ -1,13 +1,17 @@
 import styles from '../styles/Door.module.css';
 
 export default function Door(props) {
+  const isSelected = props.isSelected ? styles.selected : '';
+
   return (
     <div className={styles.area}>
-      <div className={styles.frame}>
+      <div className={`${styles.frame} ${isSelected}`}>
         <div className={styles.door}>
           <div className={styles.number}>3</div>
+          <div className={styles.knob}></div>
         </div>
       </div>
+      <div className={styles.floor}></div>
     </div>
   )
 }
