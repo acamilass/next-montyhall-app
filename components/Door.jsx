dbe01d8bcd24fb33fad3479bd32dfe90e86cd39c
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-13 16:01:29 -0300

    Adicionando elementos ao componente porta

--- components/Door.jsx
+++ components/Door.jsx
@@ -2,8 +2,12 @@ import styles from '../styles/Door.module.css';
 
 export default function Door(props) {
   return (
-    <div className={styles.door}>
-      <div className={styles.number}>3</div>
+    <div className={styles.area}>
+      <div className={styles.frame}>
+        <div className={styles.door}>
+          <div className={styles.number}>3</div>
+        </div>
+      </div>
     </div>
   )
 }
