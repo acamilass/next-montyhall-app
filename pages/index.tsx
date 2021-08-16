00ff2b12e600dbf61550b070c561672a0883b97e
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-16 09:21:58 -0300

    Criando formulário de inicializar o jogo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
+import Card from '../components/Card';
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div style={{display: "flex"}}>
-      <h1>Inicio do Jogo</h1>
+    <div className={styles.home}>
+      <div className="">
+        <Card bgColor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card></Card>
+      </div>
+      <div className="">
+        <Card></Card>
+        <Card bgColor="#28a085">
+          <Link href={`/game/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
     </div>
   )
 }
